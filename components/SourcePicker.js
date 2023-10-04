e1f7daf7a13a9980e6b8242b5f55138b045b852f
Charlie Holtz <charlieholtz@gmail.com>
2023-10-04 11:12:35 -0400

    update source picker options

--- components/SourcePicker.js
+++ components/SourcePicker.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const SourcePicker = ({ onSourceChange, selectedSources }) => {
-  const allSources = ["Replicate", "GitHub", "HuggingFace", "Reddit"];
+  const allSources = ["GitHub", "Replicate", "HuggingFace", "Reddit"];
 
   const handleChange = (e) => {
     const value = e.target.value;
