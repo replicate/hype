b24eee33206c65a2ba98b04ada6bdee6e6e41a7e
Andreas Jansson <ajansson@cloudflare.com>
2025-12-26 12:26:59 +0100

    Remove html.ts and supabase.ts (replaced by db.ts)

--- src/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
-
-export function getSupabase(env: Env): SupabaseClient {
-	return createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
-}
